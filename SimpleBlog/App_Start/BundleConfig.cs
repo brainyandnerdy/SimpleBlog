07d17fa7889f78018a48d9d30d2dcdbb5a338923
lau-PC\lau <vladoiu_lau@yahoo.com>
2017-10-20 20:31:20 +0300

    Basic admin layout

--- SimpleBlog/App_Start/BundleConfig.cs
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace SimpleBlog
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/bootstrap.css")
+                .Include("~/content/admin.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -22,10 +26,12 @@ namespace SimpleBlog
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+            
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+                      
         }
     }
 }
