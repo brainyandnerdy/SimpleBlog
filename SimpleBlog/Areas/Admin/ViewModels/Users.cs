d46d9fde1f6a16d154be2c0a5943e5c3df7dc42a
lau-PC\lau <vladoiu_lau@yahoo.com>
2017-10-24 20:41:07 +0300

    Made the edit button to work

--- SimpleBlog/Areas/Admin/ViewModels/Users.cs
+++ SimpleBlog/Areas/Admin/ViewModels/Users.cs
@@ -23,4 +23,13 @@ namespace SimpleBlog.Areas.Admin.ViewModels
         [Required, MaxLength(256), DataType(DataType.EmailAddress)]
         public string Email { get; set; }
     }
+
+    public class UsersEdit
+    {
+        [Required, MaxLength(128)]
+        public string Username { get; set; }        
+
+        [Required, MaxLength(256), DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
 }
